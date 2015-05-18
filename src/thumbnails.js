0c27396b354e9e19f57fcf76221f48deff22a5fd
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-05-18 22:53:57 +0200

    Add thumbnails to the videos

--- src/thumbnails.js
+++ src/thumbnails.js
@@ -3,14 +3,37 @@ function Thumbnails() {
   this.version = '@VERSION@';
   this.name = 'InstaSynchP Thumbnails';
   this.styles = [{
-    name: 'core',
+    name: 'thumbnails',
     url: '@RAWGITREPO@/@THUMBNAILSCSSREV@/dist/thumbnails.css',
     autoload: true
   }];
+  //TODO add setting to hide thumbnails
 }
+
+Thumbnails.prototype.onAddVideo = function (video) {
+  'use strict';
+  var videoIndex = window.room.playlist.indexOf(video.info) + 1;
+  var $video = $('#playlist > li:nth-child({0})'.format(videoIndex));
+  var $thumbnail = $('<a>', {
+    href: urlParser.create({
+      videoInfo: video.info
+    }),
+    target: '_blank'
+  }).append(
+    $('<img>', {
+      src: video.info.thumbnail,
+      class: 'video-thumbnail'
+    })
+  );
+
+  $video.find('.title').before($thumbnail);
+};
 
 Thumbnails.prototype.executeOnce = function () {
   'use strict';
+
+  var _this = this;
+  events.on(_this, 'AddVideo', _this.onAddVideo);
 };
 
 window.plugins = window.plugins || {};
